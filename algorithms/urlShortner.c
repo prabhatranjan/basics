1dadf91f39b06a1c1c2a401e951541f3395e984d
PRABHAT RANJAN <prabhatster@gmail.com>
2018-03-13 12:33:52 +0800

    initialization

--- algorithms/urlShortner.c
+++ algorithms/urlShortner.c
@@ -19,7 +19,7 @@ int main(){
 void shorten(int id){
   char base[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   char sh[6];
-  int i;
+  int i=0;
   while(id){
     sh[i]=base[id%62];
     id=id/62;
